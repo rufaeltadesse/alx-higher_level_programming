34816ff52b29f79ce3ed43d1647841c1a496f120
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-09 06:38:30 -0700

    PyCode

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 temp = 0
 for char in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(char - temp)), end="")
-    if temp == 0: 
-        temp = 32 
+    if temp == 0:
+        temp = 32
     else:
         temp = 0
