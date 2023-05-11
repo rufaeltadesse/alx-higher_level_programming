f942452392da80aa278bc1c738b0bea9bb60e3c3
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-11 02:07:26 -0700

    Three Qustions

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv)-1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv)-1))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv)-1))
+        for x in range(len(sys.argv)-1):
+            print("{}: {}".format(x+1, sys.argv[x+1]))
