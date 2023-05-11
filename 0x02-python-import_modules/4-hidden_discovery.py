309b470c9a71ff6e02a1e12df5f646c08cbb0f78
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-11 02:57:51 -0700

    Hidden Disovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ import hidden_4
 if __name__ == "__main__":
     sortedStr = sorted(dir(hidden_4))
     for x in range(len(sortedStr)):
-        if sortedStr[x].startswith("__") == False:
+        if sortedStr[x].startswith("__") is False:
             print(sortedStr[x])
