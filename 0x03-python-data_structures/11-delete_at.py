9a090fa97fa81b55d363631a508d90a36ff0cbcf
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-15 01:32:14 -0700

    djust

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if len(my_list) < 0 or idx >= len(my_list):
         return
-    for x in range(len(my_list)):
-        if x == idx:
-            del my_list[x]
+    del my_list[idx]
     return my_list
