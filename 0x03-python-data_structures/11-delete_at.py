9fc3c2a298a28877254fc1795e5feda190bde05c
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-15 01:29:34 -0700

    Adjustment

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,11 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if len(my_list) < 0 or idx >= len(my_list):
         return
-    new_list = []
     for x in range(len(my_list)):
-        if x != idx:
-            new_list.append(my_list[x])
-    my_list.clear()
-    for x in new_list:
-        my_list.append(x)
-    return new_list
+        if x == idx:
+            del my_list[x]
+    return my_list
