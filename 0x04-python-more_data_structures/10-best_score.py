4c7a66fe8bbc8f0ee2c95f73bce4fc9259752046
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-17 09:43:10 -0700

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+    else:
+        keys = a_dictionary.keys()
+        firstData = 1
+        maxValue = 0
+        maxReturn = ""
+        for i in keys:
+            if firstData == 1:
+                maxValue = a_dictionary[i]
+                maxReturn = i
+                firstData = 0
+            if maxValue < a_dictionary[i]:
+                maxReturn = i
+                maxValue = a_dictionary[i]
+        return maxReturn       
