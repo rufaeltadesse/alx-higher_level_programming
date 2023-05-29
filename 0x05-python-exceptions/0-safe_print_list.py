998287ea777f5b83e15817b768cad3f77438d1de
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-29 02:28:55 -0700

    Safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        size = 0
+        for i in my_list:
+            print(i, end='')
+            size = size + 1
+            if i == x:
+                break
+        print()
+        return(my_list[x-1])
+    except IndexError:
+        return my_list[size-1]
