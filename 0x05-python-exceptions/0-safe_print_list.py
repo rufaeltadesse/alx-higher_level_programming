e65ddd35b998baffa94125d9efbe8ffc39b6e3ec
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-29 06:37:34 -0700

    Safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             if i == x:
                 break
         print()
-        return(my_list[x-1])
+        return size
     except IndexError:
-        return my_list[size-1]
+        return size
