c4b333010cf55fc3c126fe52ceb7dd1665f7dfd9
rufaeltadesse <rufael.tadesse@gmail.com>
2023-05-30 04:31:02 -0700

    Squre 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining a square class"""
+
+
+class Square:
+    """Defining square attributes"""
+
+    def __init__(self, size):
+        """Initializing the square"""
+        self.__size = size
