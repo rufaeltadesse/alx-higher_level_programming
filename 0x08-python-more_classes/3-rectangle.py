dad92473c5b7fea727f054acab1e9e57492e352b
rufaeltadesse <rufael.tadesse@gmail.com>
2023-06-05 04:17:55 -0700

    Rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Defining a reactangle class."""
+
+
+class Rectangle:
+    """Rectangle class rep."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize new rect.
+
+        Args:
+            width (int): width of the new rectangle
+            height (int): height of the new rect
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rect"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of rect """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the Rect."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Return the perimeter of the Rect."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return the printable rep of the Rectangle.
+        """
+        if self.__height == 0 or self.__width == 0:
+            return ("")
+
+        racta = []
+        for i in range(self.__height):
+            [racta.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                racta.append("\n")
+        return ("".join(racta))
