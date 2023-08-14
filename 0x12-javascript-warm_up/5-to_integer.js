d5f57a26beca2776f2bac0ad04cb6098482a29d7
rufaeltadesse <rufael.tadesse@gmail.com>
2023-08-14 07:43:07 -0700

    Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
